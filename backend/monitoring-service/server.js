fed77ca5637a0dd860b1d07ea4ebaaaeb121ae4b
Theodore07 <91310089+Theodore07@users.noreply.github.com>
2025-07-21 16:31:56 +0700

    update: remove monitoring service

--- backend/monitoring-service/server.js
+++
@@ -1,8 +0,0 @@
-import app from './src/app.js'
-import dotenv from 'dotenv'
-dotenv.config()
-
-const PORT = process.env.PORT || 3002
-app.listen(PORT, () => {
-    console.log(`Monitoring service running on port ${PORT}`)
-})
--- backend/monitoring-service/src/router/monitoring.router.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import {} from '../handler/monitoring.handler.js'
-
-const router = Router()
-
-router.get("/", )
